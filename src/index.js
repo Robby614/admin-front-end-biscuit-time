dc29a8c17ed5e7d4adfebea7294f7ad9795ba16e
Brian Bross <brian.bross@gmail.com>
2019-11-04 14:28:25 -0500

    started indexjs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 const Deact = require("./libs/deact");
 
+
+function Title(text, textColor) {
+    return Deact.create("h1", { style: `background-color: pink; text-color: ${textColor}; text-align: center; font-size: 100px;` }, text);
+}
+function subTitle(text, textColor) {
+    return Deact.create("h6", { style: `background-color: pink; text-color: ${textColor}; text-align: right; font-size: 12px;` }, text);
+}
+
+function Header() {
+    return Deact.create("header", {}, [
+        Title("Biscuit Time!", "black"),
+        subTitle("you gotta risk it to get the biscuit", "red"),
+    ]);
+};
+
+const header = Header();
+
+
+
+Deact.render(header, document.querySelector('.app'));
