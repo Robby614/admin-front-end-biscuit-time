43cd0c128c68ae9cb4e4276a872748f8887e2975
Brian Bross <brian.bross@gmail.com>
2019-11-04 14:48:58 -0500

    js created header

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 const Deact = require("./libs/deact");
+const Header = require("./components/Header");
+
+Deact.render(Header(), document.querySelector('.app'));
 
 
-function Title(text, textColor) {
-    return Deact.create("h1", { style: `background-color: pink; text-color: ${textColor}; text-align: center; font-size: 100px;` }, text);
-}
-function subTitle(text, textColor) {
-    return Deact.create("h6", { style: `background-color: pink; text-color: ${textColor}; text-align: right; font-size: 12px;` }, text);
-}
-
-function Header() {
-    return Deact.create("header", {}, [
-        Title("Biscuit Time!", "black"),
-        subTitle("you gotta risk it to get the biscuit", "red"),
-    ]);
-};
-
-const header = Header();
-
-
-
-Deact.render(header, document.querySelector('.app'));
