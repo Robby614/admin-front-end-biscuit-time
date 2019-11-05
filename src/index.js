dff63f3de96d387888f7dbe815c25450e5dd0b4e
Luke Brannigan <lukebrannigan57@gmail.com>
2019-11-05 10:24:25 -0500

    rendered artist header

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const Button = require("./components/Button");
 
 Deact.render(Header(), document.querySelector('.app'));
 
+const subheader = Deact.create("h2", {}, "Artists");
+Deact.render(subheader, document.querySelector(".app"));
+
 Deact.render(Button("purple mushrooms"), document.querySelector('.app'));
+
 
 
