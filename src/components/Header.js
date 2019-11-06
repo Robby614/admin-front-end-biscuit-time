f73e586c7b94ea7fa46b0d13b8f37155a9f08f85
Brian Bross <brian.bross@gmail.com>
2019-11-06 10:41:02 -0500

    made container

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ const Deact = require("../libs/deact");
 const Title = require("./Title");
 
 function Header() {
-    return Deact.create("header", {}, [
+    return Deact.create(
+        "header",
+        {},
+        [
         Title("Biscuit Time!", "black"),
-    ]);
+        ]
+    );
 }
 
 module.exports = Header;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ const Header = require("./components/Header");
 const Button = require("./components/Button");
 const SearchButton = require("./components/SearchButton");
 
-Deact.render(Header(), document.querySelector('.app'));
+
+Deact.render(Deact.create("section", { class: `app-container` }, [Header()]), document.querySelector('.app'));
+
+
+// Deact.render(Header(), document.querySelector('.app'));
+
+
+
 
 const subheader = Deact.create("h2", {}, "Artists");
 Deact.render(subheader, document.querySelector(".app"));
 Deact.render(SearchButton(), document.querySelector('.app'));
-
-
-
-
-
-
-
 
