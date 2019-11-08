5aa1b6163cb2706044912bff3023a6baea0cabfb
Brian Bross <brian.bross@gmail.com>
2019-11-08 12:04:38 -0500

    applied stashed changes from dev

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -1,11 +1,13 @@
 const Deact = require("../libs/deact");
+const AlbumButton = require("./AlbumButton");
 
 function ArtistCard(artist) {
-    return Deact.create("section", { class: `artist-card` }, [
+    return Deact.create("section", { class: `artist-card`, }, [
             Deact.create("h3", { class: `artist-card__name`}, artist.name), 
             Deact.create("img", { class: `artist-card__imageUrl`, src:`${artist.imageUrl}`, alt:`${artist.name}`}, ""), 
             Deact.create ("h5", { class: `artist-card__record-label`}, artist.recordLabel),
-            Deact.create("h5", { class:  `artist-card__hometown`}, artist.homeTown) 
+            Deact.create("h5", { class:  `artist-card__hometown`}, artist.homeTown), 
+            Deact.render(AlbumButton())
     ]);
 }
 
