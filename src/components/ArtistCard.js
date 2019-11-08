3b3633283ace9b9bfb25b02cac31a8e53b94f1a3
BarbaraTaylor <btaylor1787@gmail.com>
2019-11-08 15:03:12 -0500

    Album Rendering through the artist, step 1

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -2,12 +2,14 @@ const Deact = require("../libs/deact");
 const AlbumButton = require("./AlbumButton");
 
 function ArtistCard(artist) {
-    return Deact.create("section", { class: `artist-card`, }, [
+    return Deact.create("section", { class: `artist-card` }, [
             Deact.create("h3", { class: `artist-card__name`}, artist.name), 
             Deact.create("img", { class: `artist-card__imageUrl`, src:`${artist.imageUrl}`, alt:`${artist.name}`}, ""), 
             Deact.create ("h5", { class: `artist-card__record-label`}, artist.recordLabel),
             Deact.create("h5", { class:  `artist-card__hometown`}, artist.homeTown), 
-            Deact.create("button", {}, "albums")
+            Deact.create("button", {}, "albums"),
+
+            Deact.create("input", {type: `hidden`, value: `${artist.id}`}, "")
             //Deact.render(Deact.create(AlbumButton(), {}, ""), ArtistCard)
     ]);
 }
