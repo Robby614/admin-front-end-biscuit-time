49dafbd8670a2f8a39a0a722f1953be7a225aa2a
Brian Bross <brian.bross@gmail.com>
2019-11-08 11:03:46 -0500

    rendered artist image with url

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -3,9 +3,9 @@ const Deact = require("../libs/deact");
 function ArtistCard(artist) {
     return Deact.create("section", { class: `artist-card` }, [
             Deact.create("h3", { class: `artist-card__name`}, artist.name), 
-            Deact.create("h3", { class: `artist-card__albums`}, artist.albums), 
-            Deact.create ("h3", { class: `artist-card__record-label`}, artist.recordLabel),
-            Deact.create("h3", { class:  `artist-card__hometown`}, artist.homeTown) 
+            Deact.create("img", { class: `artist-card__imageUrl`, src:`${artist.imageUrl}`, alt:`${artist.name}`}, ""), 
+            Deact.create ("h5", { class: `artist-card__record-label`}, artist.recordLabel),
+            Deact.create("h5", { class:  `artist-card__hometown`}, artist.homeTown) 
     ]);
 }
 
