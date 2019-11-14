a0aa58860cef7beea2a95fa1b398231abfaa3618
Brian Bross <brian.bross@gmail.com>
2019-11-13 22:29:50 -0500

    whacka mole on song button

--- src/components/AlbumCards.js
+++ src/components/AlbumCards.js
@@ -2,13 +2,14 @@ const Deact = require("../libs/deact");
 const AlbumCard = require("./AlbumCard");
 
 function AlbumCards(artist) {
-    const albumCards = Deact.create("section", { class: `album-cards` }, "");
-    artist.albums.forEach( album => {
-            let albumCard =  Deact.create("section", { class: `album-card` }, album.title)
-            albumCards.appendChild(albumCard);
-        }
-    )
-    return albumCards;
-}
+//     const albumCards = Deact.create("section", { class: `album-cards` }, "");
+//     artist.albums.forEach( album => {
+//             let albumCard =  Deact.create("section", { class: `album-card` }, album.title)
+//             albumCards.appendChild(albumCard);
+//         }
+//     )
+//     return albumCards;
+// }
+    return Deact.create("section", {class: 'album-cards'}, "");}
 
 module.exports = AlbumCards;
